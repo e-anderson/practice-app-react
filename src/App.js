c052b5e1a537f13699cb433fb16558a597f682ae
Erica <elander7@asu.edu>
2019-06-14 14:34:30 -0700

    commit before i mess up

--- src/App.js
+++ src/App.js
@@ -3,6 +3,26 @@ import List from "./js/components/List";
 import Form from "./js/components/Form";
 import Post from "./js/components/Post";
 
+const { Client } = require('pg');
+
+var DATABASE_URL ="postgres://rmxmpzgcmwwzzs:3206e5e9975d89cfce5819a3a9d673762d703e696ac72739cf9c1dde3edc8253@ec2-54-227-245-146.compute-1.amazonaws.com:5432/ddcu08p8i3oma3";
+
+const client = new Client({
+    connectionString: DATABASE_URL,
+    ssl: true,
+});
+
+client.connect();
+
+client.query('SELECT table_schema,table_name FROM information_schema.tables;', (err, res) => {
+    if (err) throw err;
+    for (let row of res.rows) {
+        console.log(JSON.stringify(row));
+    }
+    client.end();
+});
+
+
 const App = () => (
     <div className="row mt-5">
       <div className="col-md-4 offset-md-1">
--- src/index.js
+++ src/index.js
@@ -3,9 +3,11 @@ import { render } from "react-dom";
 import { Provider } from "react-redux";
 import store from "./js/store/index"
 import App from './App'
+
 // if you're in create-react-app import the files as:
 // import store from "./js/store/index";
 // import App from "./js/components/App.jsx";
+
 render(
     <Provider store={store}>
         <App />
